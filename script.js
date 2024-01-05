0676b7c43e90cec6e3ecb899ce04e794f4e9d208
Linfindel <116459177+linfindel@users.noreply.github.com>
2024-01-05 10:08:34 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const container = document.getElementById("container");
+let score = 0;
+let answer;
+
+document.getElementById("highscore").innerText = `Highscore: ${localStorage.getItem("highscore") || 0}`;
+
+function start() {
+  container.style.opacity = "0";
+
+  setTimeout(() => {
+    container.innerHTML = `
+      <div class="card column">
+        <h1 id="question"></h1>
+
+        <input id="input" type="text" placeholder="Answer here" oninput="mark()" autofocus>
+      </div>
+    `;
+
+    container.style.opacity = "1";
+
+    document.getElementById("progress").style.animation = "10s linear progress-determinate reverse both";
+
+    ask();
+
+    setTimeout(() => {
+      end();
+    }, 10000);
+  }, 250);
+}
+
+function ask() {
+  document.getElementById("input").value = "";
+
+  const num1 = Math.floor(Math.random() * 10);
+  const num2 = Math.floor(Math.random() * 10);
+
+  answer = num1 + num2;
+
+  document.getElementById("question").innerText = `${num1} + ${num2}`;
+}
+
+function mark() {
+  const input = document.getElementById("input").value;
+
+  if (input == answer) {
+    ask();
+    
+    score++;
+  }
+}
+
+function end() {
+  if (score > localStorage.getItem("highscore")) {
+    localStorage.setItem("highscore", score);
+  }
+
+  location.reload();
+}
+
+function reset() {
+  localStorage.clear();
+  location.reload();
+}

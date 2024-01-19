d022933ff42b7d83c6c7caef5faacfd0552222e4
Linfindel <116459177+linfindel@users.noreply.github.com>
2024-01-19 09:46:19 +0000

    Add release notes system

---
+++ about.js
@@ -0,0 +1,24 @@
+const username = 'linfindel';
+const repo = 'maths';
+
+fetch(`https://api.github.com/repos/${username}/${repo}/commits?per_page=1`)
+.then(response => {
+  const totalCount = response.headers.get('Link').match(/page=(\d+)>; rel="last"/)[1] / 100;
+  return response.json().then(data => {
+    const latestCommitMessage = data[0].commit.message;
+    console.log(`Total commit count: ${totalCount}`);
+    console.log(`Latest commit message: ${latestCommitMessage}`);
+      
+    document.getElementById("version").innerText = `Version ${totalCount} Release Notes`;
+    document.getElementById("whats-new").innerText = latestCommitMessage;
+  });
+})
+.catch(error => console.error('Error fetching data:', error));
+
+function back() {
+  document.getElementById("about-card").style.animation = "1s slide-out ease-in both";
+
+  setTimeout(() => {
+    location.href = ".";
+  }, 1000);
+}
